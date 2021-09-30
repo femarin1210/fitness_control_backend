7f68f8c39af8ad6857088cec9334bfea82a08738
femarin1210 <fmarin@rede.ulbra.br>
2021-09-30 16:25:58 -0300

    migration assessment

---
+++ database/migrations/2021_09_30_163415_create_assessment_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssessmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessment', function (Blueprint $table) {
+            $table->id();
+            $table->string('type',1);
+            $table->string('title');
+            $table->date('date');
+            $table->integer('height');
+            $table->integer('weight');
+            $table->double('fat_percentage');
+            $table->integer('chest');
+            $table->integer('biceps');
+            $table->integer('waist');
+            $table->integer('hip');
+            $table->integer('thigh');
+            $table->integer('calf');
+            $table->bigInteger('id_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workouts');
+    }
+}
